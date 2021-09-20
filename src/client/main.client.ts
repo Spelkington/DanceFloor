fa5f3d07d73a57814b1251c898b6c3abd423674d
Spencer Elkington <spelkington@gmail.com>
2021-09-19 23:55:28 -0700

    base project deps

---
+++ src/client/main.client.ts
@@ -0,0 +1,4 @@
+import { makeHello } from "shared/module";
+import { KnitClient as Knit } from "@rbxts/knit";
+
+print(makeHello("main.client.ts"));
---
+++ src/server/main.server.ts
@@ -0,0 +1,4 @@
+import { makeHello } from "shared/module";
+import { KnitServer as Knit } from "@rbxts/knit";
+
+print(makeHello("main.server.ts"));
